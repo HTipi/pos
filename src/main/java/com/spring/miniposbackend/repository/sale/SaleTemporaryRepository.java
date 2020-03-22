16f3b49a8a445dd7dc399c93eef851b5f00e3951
Tipi Hing <hingtipi@gmail.com>
2020-03-22 15:50:50 +0700

    Delete saletmpbyid

--- src/main/java/com/spring/miniposbackend/repository/sale/SaleTemporaryRepository.java
+++ src/main/java/com/spring/miniposbackend/repository/sale/SaleTemporaryRepository.java
@@ -4,6 +4,7 @@ package com.spring.miniposbackend.repository.sale;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -37,4 +38,9 @@ public interface SaleTemporaryRepository extends JpaRepository<SaleTemporary, Lo
 
     @Query(value = "delete from SaleTemporary s where s.seat.id=?1")
     List<SaleTemporary> deleteBySeatId(Integer seatId);
+    
+    
+    @Modifying
+    @Query(value = "delete from Sales_temp where id=?1", nativeQuery = true)
+    void deleteBySaleTempId(Long saleTmpId);
 }

4cbd42ce13a4b71a6bf35fe3e0297fe5270a535a
HTipi <HTipi@KHL-5CG95171NY.dli-cambodia.local>
2020-04-08 17:33:42 +0700

    Sale Transaction by Sale Id

--- src/main/java/com/spring/miniposbackend/repository/sale/SaleRepository.java
+++ src/main/java/com/spring/miniposbackend/repository/sale/SaleRepository.java
@@ -3,6 +3,7 @@ package com.spring.miniposbackend.repository.sale;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.spring.miniposbackend.model.sale.Sale;
@@ -11,6 +12,10 @@ import com.spring.miniposbackend.model.sale.Sale;
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 	 List<Sale> findByBranchId(Integer branchId);
 	 List<Sale> findByUserId(Integer userId);
+	 
+	 @Query(value = "select s from Sale s where s.user.id = ?1 and s.id = ?2")
+	    List<Sale> findByIdWithUserId(Integer userId, Long saleId);
+	 
 	 
 
 }
